d34428745b1ce5fec981adb085bfc541f7d46fd1
Courtney <courtneyobeirne@gmail.com>
2021-11-13 14:54:37 +0000

    Finished rules

--- RotaChecker/Classes/RotaBuilder.cs
+++ RotaChecker/Classes/RotaBuilder.cs
@@ -11,24 +11,38 @@ namespace RotaChecker
             //Need validation for this - make sure shifts not overlapping, that end time is after start time etc.
             Rota rota = new Rota();
 
-            rota.AddShift(new Shift(new DateTime(2021, 10, 10, 9, 0, 0), new DateTime(2021, 10, 10, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 11, 7, 30, 0), new DateTime(2021, 10, 11, 19, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 12, 8, 0, 0), new DateTime(2021, 10, 12, 21, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 13, 9, 0, 0), new DateTime(2021, 10, 13, 17, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 14, 7, 0, 0), new DateTime(2021, 10, 14, 13, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 16, 12, 0, 0), new DateTime(2021, 10, 16, 20, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 1, 9, 0, 0), new DateTime(2021, 11, 1, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 2, 9, 0, 0), new DateTime(2021, 11, 2, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 3, 9, 0, 0), new DateTime(2021, 11, 3, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 4, 9, 0, 0), new DateTime(2021, 11, 4, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 5, 9, 0, 0), new DateTime(2021, 11, 5, 17, 0, 0)));
 
-            rota.AddShift(new Shift(new DateTime(2021, 10, 20, 12, 0, 0), new DateTime(2021, 10, 20, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 21, 12, 0, 0), new DateTime(2021, 10, 21, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 22, 12, 0, 0), new DateTime(2021, 10, 22, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 23, 12, 0, 0), new DateTime(2021, 10, 23, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 24, 12, 0, 0), new DateTime(2021, 10, 24, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 25, 12, 0, 0), new DateTime(2021, 10, 25, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 26, 12, 0, 0), new DateTime(2021, 10, 26, 20, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 10, 28, 9, 0, 0), new DateTime(2021, 10, 28, 20, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 8, 9, 0, 0), new DateTime(2021, 11, 8, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 9, 9, 0, 0), new DateTime(2021, 11, 9, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 10, 9, 0, 0), new DateTime(2021, 11, 10, 17, 0, 0)));
+            rota.AddOnCall(new OnCallPeriod(new DateTime(2021, 11, 10, 16, 0, 0), new DateTime(2021, 11, 11, 9, 0, 0), new TimeSpan(4, 30, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 11, 9, 0, 0), new DateTime(2021, 11, 11, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 12, 9, 0, 0), new DateTime(2021, 11, 12, 17, 0, 0)));
 
-            rota.AddShift(new Shift(new DateTime(2021, 11, 20, 23, 0, 0), new DateTime(2021, 11, 21, 6, 0, 0)));
-            rota.AddShift(new Shift(new DateTime(2021, 11, 23, 7, 30, 0), new DateTime(2021, 11, 22, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 15, 9, 0, 0), new DateTime(2021, 11, 15, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 16, 9, 0, 0), new DateTime(2021, 11, 16, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 17, 9, 0, 0), new DateTime(2021, 11, 17, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 18, 9, 0, 0), new DateTime(2021, 11, 18, 17, 0, 0)));
+            rota.AddOnCall(new OnCallPeriod(new DateTime(2021, 11, 20, 9, 0, 0), new DateTime(2021, 11, 21, 9, 0, 0), new TimeSpan(4, 30, 0)));
+            rota.AddOnCall(new OnCallPeriod(new DateTime(2021, 11, 21, 9, 0, 0), new DateTime(2021, 11, 22, 9, 0, 0), new TimeSpan(4, 30, 0)));
+
+            rota.AddShift(new Shift(new DateTime(2021, 11, 23, 9, 0, 0), new DateTime(2021, 11, 23, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 24, 9, 0, 0), new DateTime(2021, 11, 24, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 25, 9, 0, 0), new DateTime(2021, 11, 25, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 26, 9, 0, 0), new DateTime(2021, 11, 26, 17, 0, 0)));
+            rota.AddOnCall(new OnCallPeriod(new DateTime(2021, 11, 26, 13, 0, 0), new DateTime(2021, 11, 27, 9, 0, 0), new TimeSpan(4, 30, 0)));
+
+            rota.AddShift(new Shift(new DateTime(2021, 11, 29, 9, 0, 0), new DateTime(2021, 11, 29, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 11, 30, 9, 0, 0), new DateTime(2021, 11, 30, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 12, 1, 9, 0, 0), new DateTime(2021, 12, 1, 17, 0, 0)));
+            rota.AddOnCall(new OnCallPeriod(new DateTime(2021, 12, 1, 16, 0, 0), new DateTime(2021, 12, 2, 9, 0, 0), new TimeSpan(4, 30, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 12, 2, 9, 0, 0), new DateTime(2021, 12, 2, 17, 0, 0)));
+            rota.AddShift(new Shift(new DateTime(2021, 12, 3, 9, 0, 0), new DateTime(2021, 12, 3, 17, 0, 0)));
 
             return rota;
 
