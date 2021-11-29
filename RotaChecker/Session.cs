b5df8651b7e296d41980af678b1aee91572a04da
Courtney <courtneyobeirne@gmail.com>
2021-11-29 22:48:10 +0000

    removed unused code

--- RotaChecker/Session.cs
+++ RotaChecker/Session.cs
@@ -10,22 +10,12 @@ namespace RotaChecker
 
         public Rota CurrentRota { get; }
         public Month CurrentMonth { get; set; }
-        public List<DayEvents> DutiesInMonth { get; set; } = new List<DayEvents>();
         public Session()
         {
             CurrentRota = RotaBuilder.CreateRota();
 
             CurrentMonth = new Month(CurrentRota.RotaStartTime);
 
-        }
-
-        public void GetDutiesInCurrentMonth()
-        {
-            foreach (DateTime date in CurrentMonth.DaysInMonth)
-            {
-                DayEvents day = new DayEvents(date);
-                day.Duties = CurrentRota.GetDutiesOnDate(day.Date);
-            }
         }
 
     }
